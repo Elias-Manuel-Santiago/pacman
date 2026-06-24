6b04921c7821f21a71f3cdbdaa9803e41e269a0a
Silva Lucas <lucassilvabadell@gmail.com>
2026-06-24 15:05:20 -0300

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -5,13 +5,38 @@ import { injectRetroStyles } from './UI/stylesManager.js';
 import { MenuHTML } from './UI/menuHTML.js';
 import { VersusGame } from './engine/VersusGame.js';
 import { DJ, SOUND_GROUP } from './sfx/DJ.js';
-// IMPORTAMOS EL NUEVO ALMACENAMIENTO DE DATOS
 import { updateVisualLeaderboard, submitSingleplayerScore } from './engine/LeaderboardStorage.js';
 
+// Solo importamos el archivo que sabemos que existe físicamente en la carpeta sfx/
 const levelThemeUrl = new URL('./sfx/level-theme.mp3', import.meta.url).href;
 
+// COMENTADOS: Cuando se agreguen estos archivos a la carpeta src/sfx/, descomentar las líneas:
+// const eatSfxUrl = new URL('./sfx/eat.ogg', import.meta.url).href;
+// const levelUpSfxUrl = new URL('./sfx/levelup.wav', import.meta.url).href;
+// const move1Url = new URL('./sfx/move1.wav', import.meta.url).href;
+// const move2Url = new URL('./sfx/move2.wav', import.meta.url).href;
+// const move3Url = new URL('./sfx/move3.wav', import.meta.url).href;
+// const menuClick = new URL('./sfx/menuClick.wav', import.meta.url).href;
+// const win = new URL('./sfx/win.wav', import.meta.url).href;
+// const gameOver = new URL('./sfx/gameOver.wav', import.meta.url).href;
+
 DJ.registerMany({
+    // Mapeo de nombres exactos que solicita Game.js. Se usa '' (vacío) como fuente para que Webpack compile sin errores.
+    'orb': {src: '', group: SOUND_GROUP.SFX, loop: false, volume: 0.5},
+    'pellet': {src: '', group: SOUND_GROUP.SFX, loop: false, volume: 0.5},
+    'ghost-eaten': {src: '', group: SOUND_GROUP.SFX, loop: false, volume: 0.6},
+    'death': {src: '', group: SOUND_GROUP.SFX, loop: false, volume: 0.7},
+    'game-over': {src: '', group: SOUND_GROUP.SFX, loop: false, volume: 0.7},
+    'level-clear': {src: '', group: SOUND_GROUP.SFX, loop: false, volume: 0.7},
+    'win': {src: '', group: SOUND_GROUP.SFX, loop: false, volume: 0.7},
     'level-theme': { src: levelThemeUrl, group: SOUND_GROUP.MUSIC, loop: true, volume: 0.7 },
+    
+    'eat': {src: '', group: SOUND_GROUP.SFX, loop: false, volume: 0.5},
+    'levelup': {src: '', group: SOUND_GROUP.SFX, loop: false, volume: 0.7},
+    'move1': {src: '', group: SOUND_GROUP.SFX, loop: false, volume: 0.2},
+    'move2': {src: '', group: SOUND_GROUP.SFX, loop: false, volume: 0.2},
+    'move3': {src: '', group: SOUND_GROUP.SFX, loop: false, volume: 0.2},
+    'menuClick': {src: '', group: SOUND_GROUP.SFX, loop: false, volume: 0.2},
 });
 
 (async () => {
@@ -66,6 +91,13 @@ DJ.registerMany({
             btnReady.onclick = () => {
                 overlay.classList.add('hidden');
 
+                // Desbloquear AudioContext en dispositivos móviles
+                if (typeof DJ.resume === 'function') {
+                    DJ.resume();
+                } else if (DJ.ctx && typeof DJ.ctx.resume === 'function') {
+                    DJ.ctx.resume();
+                }
+
                 const game = new Game(app, {
                     isVersus: false,
                     player1: gameSettings.player1,
@@ -101,7 +133,7 @@ DJ.registerMany({
                     let isNewRecord = false;
                     if (!isVersus) {
                         isNewRecord = submitSingleplayerScore(gameSettings.player1, score);
-                        game.ui.renderLeaderboard(getLeaderboard());
+                        updateVisualLeaderboard();
                     }
                     originalShowWin(score, isVersus, isFinalTournament, isMaxLevel, actions, isNewRecord);
                 };
